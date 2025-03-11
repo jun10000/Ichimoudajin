bd5a1a9bb234bcc17441472040db7ab46918cb93
jun10000 <39115187+jun10000@users.noreply.github.com>
2025-03-11 19:50:21 +0900

    Refactor

--- ebitenhelper/actor/tilemap.go
+++ ebitenhelper/actor/tilemap.go
@@ -113,150 +113,164 @@ type TileMap struct {
 	ObjectLayers []TileMapObjectLayer
 }
 
+func (o *tileMapObjectLayerObjectXML) CreatePawn() (*Pawn, error) {
+	ret := NewPawn()
+	ret.SetLocation(utility.NewVector(o.LocationX, o.LocationY))
+	ret.FrameSize.X = int(o.SizeX)
+	ret.FrameSize.Y = int(o.SizeY)
+
+	for _, property := range o.Properties {
+		switch property.Name {
+		case "Accel":
+			err := utility.StringToFloat(property.Value, &ret.Accel)
+			if err != nil {
+				return nil, err
+			}
+		case "Decel":
+			err := utility.StringToFloat(property.Value, &ret.Decel)
+			if err != nil {
+				return nil, err
+			}
+		case "FPS":
+			err := utility.StringToInt(property.Value, &ret.FPS)
+			if err != nil {
+				return nil, err
+			}
+		case "FrameCount":
+			err := utility.StringToInt(property.Value, &ret.FrameCount)
+			if err != nil {
+				return nil, err
+			}
+		case "FrameDirectionMap":
+			clear(ret.FrameDirectionMap)
+			for _, v := range property.Value {
+				ret.FrameDirectionMap = append(ret.FrameDirectionMap, utility.RuneToInt(v))
+			}
+		case "Image":
+			img, err := utility.GetImageFromFile(property.Value)
+			if err != nil {
+				return nil, err
+			}
+			ret.Image = img
+		case "MaxSpeed":
+			err := utility.StringToFloat(property.Value, &ret.MaxSpeed)
+			if err != nil {
+				return nil, err
+			}
+		case "RotationDeg":
+			var deg float64
+			err := utility.StringToFloat(property.Value, &deg)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetRotation(utility.DegreeToRadian(deg))
+		case "ScaleX":
+			s := ret.GetScale()
+			err := utility.StringToFloat(property.Value, &s.X)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetScale(s)
+		case "ScaleY":
+			s := ret.GetScale()
+			err := utility.StringToFloat(property.Value, &s.Y)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetScale(s)
+		default:
+			log.Printf("Found unknown Tiled object (%s) property: %s = %s\n",
+				o.Name, property.Name, property.Value)
+		}
+	}
+
+	return ret, nil
+}
+
+func (o *tileMapObjectLayerObjectXML) CreateAIPawn() (*AIPawn, error) {
+	ret := NewAIPawn()
+	ret.SetLocation(utility.NewVector(o.LocationX, o.LocationY))
+	ret.FrameSize.X = int(o.SizeX)
+	ret.FrameSize.Y = int(o.SizeY)
+
+	for _, property := range o.Properties {
+		switch property.Name {
+		case "Accel":
+			err := utility.StringToFloat(property.Value, &ret.Accel)
+			if err != nil {
+				return nil, err
+			}
+		case "Decel":
+			err := utility.StringToFloat(property.Value, &ret.Decel)
+			if err != nil {
+				return nil, err
+			}
+		case "FPS":
+			err := utility.StringToInt(property.Value, &ret.FPS)
+			if err != nil {
+				return nil, err
+			}
+		case "FrameCount":
+			err := utility.StringToInt(property.Value, &ret.FrameCount)
+			if err != nil {
+				return nil, err
+			}
+		case "FrameDirectionMap":
+			clear(ret.FrameDirectionMap)
+			for _, v := range property.Value {
+				ret.FrameDirectionMap = append(ret.FrameDirectionMap, utility.RuneToInt(v))
+			}
+		case "Image":
+			img, err := utility.GetImageFromFile(property.Value)
+			if err != nil {
+				return nil, err
+			}
+			ret.Image = img
+		case "MaxSpeed":
+			err := utility.StringToFloat(property.Value, &ret.MaxSpeed)
+			if err != nil {
+				return nil, err
+			}
+		case "RotationDeg":
+			var deg float64
+			err := utility.StringToFloat(property.Value, &deg)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetRotation(utility.DegreeToRadian(deg))
+		case "ScaleX":
+			s := ret.GetScale()
+			err := utility.StringToFloat(property.Value, &s.X)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetScale(s)
+		case "ScaleY":
+			s := ret.GetScale()
+			err := utility.StringToFloat(property.Value, &s.Y)
+			if err != nil {
+				return nil, err
+			}
+			ret.SetScale(s)
+		default:
+			log.Printf("Found unknown Tiled object (%s) property: %s = %s\n",
+				o.Name, property.Name, property.Value)
+		}
+	}
+
+	return ret, nil
+}
+
 func (o *tileMapObjectLayerObjectXML) CreateActor() (any, error) {
 	switch o.Class {
 	case "Pawn":
-		ret := NewPawn()
-		ret.SetLocation(utility.NewVector(o.LocationX, o.LocationY))
-		ret.FrameSize.X = int(o.SizeX)
-		ret.FrameSize.Y = int(o.SizeY)
-
-		for _, property := range o.Properties {
-			switch property.Name {
-			case "Accel":
-				err := utility.StringToFloat(property.Value, &ret.Accel)
-				if err != nil {
-					return nil, err
-				}
-			case "Decel":
-				err := utility.StringToFloat(property.Value, &ret.Decel)
-				if err != nil {
-					return nil, err
-				}
-			case "FPS":
-				err := utility.StringToInt(property.Value, &ret.FPS)
-				if err != nil {
-					return nil, err
-				}
-			case "FrameCount":
-				err := utility.StringToInt(property.Value, &ret.FrameCount)
-				if err != nil {
-					return nil, err
-				}
-			case "FrameDirectionMap":
-				clear(ret.FrameDirectionMap)
-				for _, v := range property.Value {
-					ret.FrameDirectionMap = append(ret.FrameDirectionMap, utility.RuneToInt(v))
-				}
-			case "Image":
-				img, err := utility.GetImageFromFile(property.Value)
-				if err != nil {
-					return nil, err
-				}
-				ret.Image = img
-			case "MaxSpeed":
-				err := utility.StringToFloat(property.Value, &ret.MaxSpeed)
-				if err != nil {
-					return nil, err
-				}
-			case "RotationDeg":
-				var deg float64
-				err := utility.StringToFloat(property.Value, &deg)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetRotation(utility.DegreeToRadian(deg))
-			case "ScaleX":
-				s := ret.GetScale()
-				err := utility.StringToFloat(property.Value, &s.X)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetScale(s)
-			case "ScaleY":
-				s := ret.GetScale()
-				err := utility.StringToFloat(property.Value, &s.Y)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetScale(s)
-			}
-		}
-
-		return ret, nil
+		return o.CreatePawn()
 	case "AIPawn":
-		ret := NewAIPawn()
-		ret.SetLocation(utility.NewVector(o.LocationX, o.LocationY))
-		ret.FrameSize.X = int(o.SizeX)
-		ret.FrameSize.Y = int(o.SizeY)
-
-		for _, property := range o.Properties {
-			switch property.Name {
-			case "Accel":
-				err := utility.StringToFloat(property.Value, &ret.Accel)
-				if err != nil {
-					return nil, err
-				}
-			case "Decel":
-				err := utility.StringToFloat(property.Value, &ret.Decel)
-				if err != nil {
-					return nil, err
-				}
-			case "FPS":
-				err := utility.StringToInt(property.Value, &ret.FPS)
-				if err != nil {
-					return nil, err
-				}
-			case "FrameCount":
-				err := utility.StringToInt(property.Value, &ret.FrameCount)
-				if err != nil {
-					return nil, err
-				}
-			case "FrameDirectionMap":
-				clear(ret.FrameDirectionMap)
-				for _, v := range property.Value {
-					ret.FrameDirectionMap = append(ret.FrameDirectionMap, utility.RuneToInt(v))
-				}
-			case "Image":
-				img, err := utility.GetImageFromFile(property.Value)
-				if err != nil {
-					return nil, err
-				}
-				ret.Image = img
-			case "MaxSpeed":
-				err := utility.StringToFloat(property.Value, &ret.MaxSpeed)
-				if err != nil {
-					return nil, err
-				}
-			case "RotationDeg":
-				var deg float64
-				err := utility.StringToFloat(property.Value, &deg)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetRotation(utility.DegreeToRadian(deg))
-			case "ScaleX":
-				s := ret.GetScale()
-				err := utility.StringToFloat(property.Value, &s.X)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetScale(s)
-			case "ScaleY":
-				s := ret.GetScale()
-				err := utility.StringToFloat(property.Value, &s.Y)
-				if err != nil {
-					return nil, err
-				}
-				ret.SetScale(s)
-			}
-		}
-
-		return ret, nil
+		return o.CreateAIPawn()
+	default:
+		log.Println("Found unsupported Tiled map object class: " + o.Class)
+		return nil, nil
 	}
-
-	log.Println("Found unsupported Tiled map object class: " + o.Class)
-	return nil, nil
 }
 
 func (m *TileMap) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
