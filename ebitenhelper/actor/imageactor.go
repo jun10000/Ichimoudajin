cefe4b91bb184431bdbb09f5587d74d076e6b727
jun10000 <39115187+jun10000@users.noreply.github.com>
2025-03-23 18:23:47 +0900

    Change struct name: Actor -> ImageActor

--- ebitenhelper/actor/imageactor.go
+++ ebitenhelper/actor/imageactor.go
@@ -5,13 +5,13 @@ import (
 	"github.com/jun10000/Ichimoudajin/ebitenhelper/utility"
 )
 
-type Actor struct {
+type ImageActor struct {
 	*utility.StaticTransform
 	*component.DrawImageComponent
 }
 
-func NewActor(location utility.Vector, rotation float64, scale utility.Vector) *Actor {
-	a := &Actor{}
+func NewImageActor(location utility.Vector, rotation float64, scale utility.Vector) *ImageActor {
+	a := &ImageActor{}
 	a.StaticTransform = utility.NewStaticTransform(location, rotation, scale)
 	a.DrawImageComponent = component.NewDrawImageComponent(a)
 	return a
